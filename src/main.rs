fe879c2248226c8336c2e2d6b7207b9fcb9012df
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-11-04 10:12:01 +0900

    Add: refactor KeybindsApp to modularize UI components and improve sorting functionality

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,43 @@ mod font;
 mod icons;
 mod models;
 mod parser;
+mod ui;
 
 use eframe::egui;
 use app::KeybindsApp;
 use font::setup_custom_fonts;
 
 fn main() -> Result<(), eframe::Error> {
+    let icon_data = load_icon();
+    
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([1000.0, 700.0])
-            .with_title("Hyprland Keybinds Viewer"),
+            .with_title("HyprBind")
+            .with_icon(icon_data),
         ..Default::default()
     };
 
     eframe::run_native(
-        "Hyprland Keybinds",
+        "HyprBind",
         options,
         Box::new(|cc| {
             setup_custom_fonts(&cc.egui_ctx);
             Ok(Box::new(KeybindsApp::new()))
         }),
     )
+}
+
+fn load_icon() -> egui::IconData {
+    let icon_bytes = include_bytes!("../assets/logo_hyprbind.png");
+    let image = image::load_from_memory(icon_bytes)
+        .expect("Failed to load icon")
+        .into_rgba8();
+    let (width, height) = image.dimensions();
+    
+    egui::IconData {
+        rgba: image.into_raw(),
+        width,
+        height,
+    }
 }
