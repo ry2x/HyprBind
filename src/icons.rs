e52a0675c8dbca24eb33809385557c5d269e7e38
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-01-19 02:27:50 +0900

    STYLE: Remove extra spaces in icon definitions

--- src/icons.rs
+++ src/icons.rs
@@ -2,13 +2,11 @@
 pub fn get_icon(key: &str) -> String {
     match key {
         // Modifiers
-        "SUPER" => " ".to_string(),   // Apple Command key icon
-        "SHIFT" => " 󰘶  ".to_string(), // Shift key icon
+        "SUPER" => "".to_string(),   // Apple Command key icon
+        "SHIFT" => " 󰘶 ".to_string(), // Shift key icon
 
         // Keys
-        k if k.eq_ignore_ascii_case("RETURN") || k.eq_ignore_ascii_case("ENTER") => {
-            "󰌑 ".to_string()
-        }
+        k if k.eq_ignore_ascii_case("RETURN") || k.eq_ignore_ascii_case("ENTER") => "󰌑".to_string(),
         k if k.eq_ignore_ascii_case("SEMICOLON") => ";".to_string(),
         k if k.eq_ignore_ascii_case("DELETE") => "DEL".to_string(),
         k if k.eq_ignore_ascii_case("TAB") => "TAB".to_string(),
@@ -16,20 +14,20 @@ pub fn get_icon(key: &str) -> String {
         k if k.eq_ignore_ascii_case("RIGHT") => "󰜴".to_string(),
         k if k.eq_ignore_ascii_case("UP") => "󰜷".to_string(),
         k if k.eq_ignore_ascii_case("DOWN") => "󰜮".to_string(),
-        k if k.eq_ignore_ascii_case("mouse_down") => "󱕐 ".to_string(),
-        k if k.eq_ignore_ascii_case("mouse_up") => "󱕑 ".to_string(),
-        k if k.eq_ignore_ascii_case("mouse:272") => "󰍽 ".to_string(),
-        k if k.eq_ignore_ascii_case("mouse:273") => "󰍽 ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioRaiseVolume") => " ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioLowerVolume") => " ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioMute") => " ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioMicMute") => "󰍭 ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86MonBrightnessUp") => "󰃠 ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86MonBrightnessDown") => "󰃞 ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioNext") => "󰙡 ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioPause") => " ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioPlay") => " ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioPrev") => "󰙣 ".to_string(),
+        k if k.eq_ignore_ascii_case("mouse_down") => "󱕐".to_string(),
+        k if k.eq_ignore_ascii_case("mouse_up") => "󱕑".to_string(),
+        k if k.eq_ignore_ascii_case("mouse:272") => "󰍽".to_string(),
+        k if k.eq_ignore_ascii_case("mouse:273") => "󰍽".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioRaiseVolume") => "".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioLowerVolume") => "".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioMute") => "".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioMicMute") => "󰍭".to_string(),
+        k if k.eq_ignore_ascii_case("XF86MonBrightnessUp") => "󰃠".to_string(),
+        k if k.eq_ignore_ascii_case("XF86MonBrightnessDown") => "󰃞".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioNext") => "󰙡".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioPause") => "".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioPlay") => "".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioPrev") => "󰙣".to_string(),
         _ => key.to_string(),
     }
 }
