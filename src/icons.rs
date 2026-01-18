5be432f767fae30e09fd98a72a719b882774d036
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-01-18 17:12:05 +0900

    FEAT: Update key icons to use new font characters

--- src/icons.rs
+++ src/icons.rs
@@ -2,34 +2,34 @@
 pub fn get_icon(key: &str) -> String {
     match key {
         // Modifiers
-        "SUPER" => "\u{f17a} ".to_string(), // Apple Command key icon
-        "SHIFT" => "\u{f0636} ".to_string(), // Shift key icon
+        "SUPER" => " ".to_string(),   // Apple Command key icon
+        "SHIFT" => " 󰘶  ".to_string(), // Shift key icon
 
         // Keys
         k if k.eq_ignore_ascii_case("RETURN") || k.eq_ignore_ascii_case("ENTER") => {
-            "\u{f0311} ".to_string()
+            "󰌑 ".to_string()
         }
         k if k.eq_ignore_ascii_case("SEMICOLON") => ";".to_string(),
         k if k.eq_ignore_ascii_case("DELETE") => "DEL".to_string(),
         k if k.eq_ignore_ascii_case("TAB") => "TAB".to_string(),
-        k if k.eq_ignore_ascii_case("LEFT") => "\u{f0731} ".to_string(),
-        k if k.eq_ignore_ascii_case("RIGHT") => "\u{f0734} ".to_string(),
-        k if k.eq_ignore_ascii_case("UP") => "\u{f0737} ".to_string(),
-        k if k.eq_ignore_ascii_case("DOWN") => "\u{f072e} ".to_string(),
-        k if k.eq_ignore_ascii_case("mouse_down") => "\u{f1550} ".to_string(),
-        k if k.eq_ignore_ascii_case("mouse_up") => "\u{f1551} ".to_string(),
-        k if k.eq_ignore_ascii_case("mouse:272") => "\u{f522}\u{f037d} ".to_string(),
-        k if k.eq_ignore_ascii_case("mouse:273") => "\u{f037d}\u{f522} ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioRaiseVolume") => "\u{f028} ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioLowerVolume") => "\u{f027} ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioMute") => "\u{eee8} ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioMicMute") => "\u{f036d} ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86MonBrightnessUp") => "\u{f00e0} ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86MonBrightnessDown") => "\u{f00de} ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioNext") => "\u{f0661} ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioPause") => "\u{f04c} ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioPlay") => "\u{f04b} ".to_string(),
-        k if k.eq_ignore_ascii_case("XF86AudioPrev") => "\u{f0663} ".to_string(),
+        k if k.eq_ignore_ascii_case("LEFT") => "󰜱".to_string(),
+        k if k.eq_ignore_ascii_case("RIGHT") => "󰜴".to_string(),
+        k if k.eq_ignore_ascii_case("UP") => "󰜷".to_string(),
+        k if k.eq_ignore_ascii_case("DOWN") => "󰜮".to_string(),
+        k if k.eq_ignore_ascii_case("mouse_down") => "󱕐 ".to_string(),
+        k if k.eq_ignore_ascii_case("mouse_up") => "󱕑 ".to_string(),
+        k if k.eq_ignore_ascii_case("mouse:272") => "󰍽 ".to_string(),
+        k if k.eq_ignore_ascii_case("mouse:273") => "󰍽 ".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioRaiseVolume") => " ".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioLowerVolume") => " ".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioMute") => " ".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioMicMute") => "󰍭 ".to_string(),
+        k if k.eq_ignore_ascii_case("XF86MonBrightnessUp") => "󰃠 ".to_string(),
+        k if k.eq_ignore_ascii_case("XF86MonBrightnessDown") => "󰃞 ".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioNext") => "󰙡 ".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioPause") => " ".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioPlay") => " ".to_string(),
+        k if k.eq_ignore_ascii_case("XF86AudioPrev") => "󰙣 ".to_string(),
         _ => key.to_string(),
     }
 }
